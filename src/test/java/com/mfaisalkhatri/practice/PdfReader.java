b355c47200746fb1072828773a6d729622287546
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2018-10-27 22:12:07 +0530

    Initial Commit

---
+++ src/test/java/com/mfaisalkhatri/practice/PdfReader.java
@@ -0,0 +1,63 @@
+package com.mfaisalkhatri.practice;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+/**
+ * @author Faisal
+ *
+ */
+public class PdfReader {
+
+	/**
+	 * @throws IOException
+	 */
+	@Ignore
+	@Test
+	public void test1() throws IOException {
+
+		URL url = new URL("file:///D:/Study/pdf_files/AI.pdf");
+
+		BufferedInputStream TestFile = new BufferedInputStream(url.openStream());
+		PDDocument doc = PDDocument.load(TestFile);
+		// TestPDF.parse();
+		String TestText = new PDFTextStripper().getText(doc);
+
+		doc.close();
+		TestFile.close();
+
+		System.out.println(TestText);
+		Assert.assertTrue(TestText.contains("What is Artificial Intelligence?"));
+	}
+
+	/**
+	 * @throws IOException
+	 * @throws IOException
+	 */
+	@Test
+	public void test2() throws IOException {
+
+		URL url = new URL("http://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf");
+
+		BufferedInputStream TestFile = new BufferedInputStream(url.openStream());
+		PDDocument doc = PDDocument.load(TestFile);
+		String TestText = new PDFTextStripper().getText(doc);
+
+		doc.close();
+		TestFile.close();
+
+		System.out.println(TestText);
+
+		Assert.assertTrue(
+				TestText.contains("The free Acrobat Reader is easy to download and can be freely distributed "));
+
+	}
+
+}
